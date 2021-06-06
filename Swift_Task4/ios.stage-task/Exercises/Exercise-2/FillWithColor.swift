adc4cde206e7a8c5729439b53b2caf1b77bf87ad
Timur Korolev <timurkorolev89@gmail.com>
2021-06-06 18:07:08 +0300

    feat: solution of all tasks

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,28 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        let m = image.count
+        let n = image[0].count
+        if (m < 0 || n < 0 || m > 50 || n > 50 || newColor < 0 ||
+            newColor >= 65536 || row < 0 || row > m || column < 0 || column > n) {
+            return image
+        } else {
+            let oldColor = image[row][column]
+            var coloredImage = image
+            coloredImage[row][column] = newColor
+            if (row - 1 >= 0 && coloredImage[row - 1][column] == oldColor) {
+                coloredImage = fillWithColor(coloredImage, row - 1, column, newColor)
+            }
+            if (row + 1 < m && coloredImage[row + 1][column] == oldColor) {
+                coloredImage = fillWithColor(coloredImage, row + 1, column, newColor)
+            }
+            if (column - 1 >= 0 && coloredImage[row][column - 1] == oldColor) {
+                coloredImage = fillWithColor(coloredImage, row, column - 1, newColor)
+            }
+            if (column + 1 < n && coloredImage[row][column + 1] == oldColor) {
+                coloredImage = fillWithColor(coloredImage, row, column + 1, newColor)
+            }
+            return coloredImage
+        }
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,37 +1,86 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation {
+    var usersArray = [User]()
+    var callsArray = [Call]()
+}
 
 extension CallStation: Station {
+
     func users() -> [User] {
-        []
+        return usersArray
     }
     
     func add(user: User) {
-
+        if (!usersArray.contains(user)) {
+            usersArray.append(user)
+        }
     }
     
     func remove(user: User) {
-
+       usersArray = usersArray.filter { $0 != user }
     }
     
     func execute(action: CallAction) -> CallID? {
-        nil
+        switch action {
+        case .start(from: let from, to: let to):
+            if !usersArray.contains(from) && !usersArray.contains(to) { return nil }
+            else if !usersArray.contains(to) {
+                let call = Call(id: CallID.init(), incomingUser: from, outgoingUser: to, status: .ended(reason: .error))
+                callsArray.append(call)
+                return call.id
+            }
+            else if let _ = callsArray.firstIndex(where: {($0.incomingUser == to ||
+                                                           $0.outgoingUser == to) &&
+                                                          ($0.status == .calling ||
+                                                           $0.status == .talk)}) {
+                let call = Call(id: CallID.init(), incomingUser: from, outgoingUser: to, status: .ended(reason: .userBusy))
+                callsArray.append(call)
+                return call.id
+            } else {
+                let call = Call(id: CallID.init(), incomingUser: from, outgoingUser: to, status: .calling)
+                callsArray.append(call)
+                return call.id
+            }
+            
+        case .answer(from: let from):
+            if !usersArray.contains(from) {
+                guard let index = callsArray.firstIndex(where: {$0.outgoingUser == from}) else {return nil}
+                callsArray[index].status = .ended(reason: .error)
+                return nil
+            } else {
+                guard let index = callsArray.firstIndex(where: {$0.outgoingUser == from}) else {return nil}
+                callsArray[index].status = .talk
+                return callsArray[index].id
+            }
+            
+            
+        case .end(from: let from):
+            guard let index = callsArray.firstIndex(where: {$0.incomingUser == from || $0.outgoingUser == from}) else {return nil}
+            if (callsArray[index].status == .talk) {
+                callsArray[index].status = .ended(reason: .end)
+            } else if (callsArray[index].status == .calling) {
+                callsArray[index].status = .ended(reason: .cancel)
+            }
+            
+            return callsArray[index].id
+        }
     }
     
     func calls() -> [Call] {
-        []
+        callsArray
     }
     
     func calls(user: User) -> [Call] {
-        []
+        return callsArray.filter { $0.incomingUser == user || $0.outgoingUser == user }
     }
     
     func call(id: CallID) -> Call? {
-        nil
+        return callsArray.filter { $0.id == id }.first
     }
     
     func currentCall(user: User) -> Call? {
-        nil
+        callsArray.filter {($0.incomingUser == user || $0.outgoingUser == user) &&
+        ($0.status == .calling || $0.status == .talk)}.first
     }
 }
